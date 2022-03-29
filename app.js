e6ac1ef994ed9208d9972bca35d4884281f894ad
SlavaZinevichUSC <97628779+SlavaZinevichUSC@users.noreply.github.com>
2022-03-28 23:19:37 -0700

    Add functionality

--- app.js
+++ app.js
@@ -45,17 +45,23 @@ const path = require('path');
 const axios = require('axios');
 const cors = require('cors');
 const apiToken = "c87gv3qad3i9lknto67g";
-function send(res, url, reqType){
+const dateOffset = 14;
+
+function send(res, url, reqType, toDisplayResult = false){
   console.log('requesting finhub data at url: ' + url);
   axios.get(url)
     .then(finHubData => {
       console.log(`recieved finhub ${reqType} at ` + new Date().toLocaleTimeString());
       package = new Object();
       package[reqType] = finHubData.data;
+      if(toDisplayResult){
+        console.log(finHubData.data);
+
+      }
       res.send(package);
 
     }).catch(err =>{
-      console.log('error encoutered while recieving finhub profile ', err.message);
+      console.error('ERROR IN ' + reqType + " ENCOUNTERED", err.message);
       package = new Object();
       package[reqType] = new Object();
       res.send(package)
@@ -82,6 +88,29 @@ app.get('/price/:id', (req, res) => {
 app.get('/symbols/:id', (req, res) => {
   const priceUrl = `https://finnhub.io/api/v1/search?q=${req.params.id}&token=${apiToken}`;
   send(res, priceUrl, 'symbols');
+});
+
+app.get('/news/:id', (req, res) => {
+
+  var twoWeeks = new Date();
+  twoWeeks = new Date(twoWeeks.setDate(twoWeeks.getDate() - dateOffset)).toISOString().split('T')[0];
+  console.log(twoWeeks);
+  const now = new Date(Date.now()).toISOString().split('T')[0];
+  const newsUrl = `https://finnhub.io/api/v1/company-news?symbol=${req.params.id}&from=${twoWeeks}&to=${now}&token=${apiToken}`;
+  send(res, newsUrl, 'news', true);
+});
+
+app.get('/charts/:id', (req, res) =>{
+  const now =  Math.floor(Date.now() / 1000);
+  var prev = (now - 604800);
+  var last = Math.floor(new Date(new Date().setFullYear(new Date().getFullYear() -2)).getTime() /1000);
+  const candlesUrl = `https://finnhub.io/api/v1/stock/candle?symbol=${req.params.id}&resolution=D&from=${last}&to=${now}&token=${apiToken}`
+  send(res, candlesUrl, 'charts');
+});
+
+app.get('/mentions/:id', (req, res) =>{
+  const mentionsUrl = `https://finnhub.io/api/v1/stock/social-sentiment?symbol=${req.params.id}&from=2022-01-01&token=${apiToken}`;
+  send(res, mentionsUrl, 'mentions');
 });
 
 app.listen(port, () => {
